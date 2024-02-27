214cdd0ac537c2de53b4b492eeeff248a931137a
Tanha <nusrattanha897@gmail.com>
2024-02-27 22:21:11 +0600

    css syntax, id, element selector practice

---
+++ w3schools/notes.js
@@ -0,0 +1,23 @@
+//topic_01: CSS Introduction
+/**
+ * 01. CSS stands for Cascading Style Sheet. It describes how HTML elements are to be displayed on screen. It can control the layout of multiple html pages all at once. It is used to define styles for web pages/html. 
+ */
+
+//topic_02: CSS Syntax
+/**
+ * 01. A CSS rule consists of a selector and a declaration block. The selector points to the HTML element/elements that is to be designed. The declaration block contains one or more declarations separated by semi colon and is surrounded by curly braces. Each declaration includes a CSS property and its value. 
+ * 
+ */
+
+//topic_03: CSS Selectors
+/**
+ * 01. Selectors are used to select element to style.
+ *      i. element selector: selects elements based on the element name.
+ *      ii. id selector: selects elements uses the id attribute of an element. to select an element with a specific id, write a hash or pound (#) character, followed by the id of the element.
+ *      iii. class selector: selects all the elements uses the class attribute of elements. To select elements with a specific class, write a period(.) character followed by the class name.
+ *      iv. universal selector: The universal selector(*) selects all the HTML elements of the page.
+ *      v. attribute selector
+ *      vi. Pseudo-class selector
+ *      vii. Pseudo-element selector
+ * 
+ */
