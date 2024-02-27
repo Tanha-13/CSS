4deeee689f217ace9b13e5d0609cfeaf34361470
Tanha <nusrattanha897@gmail.com>
2024-02-27 23:26:46 +0600

    adding CSS to HTML and comment

--- w3schools/notes.js
+++ w3schools/notes.js
@@ -21,3 +21,16 @@
  *      vii. Pseudo-element selector
  * 
  */
+
+// topic_04: Adding CSS to HTML and Comments
+/**
+ * Three ways to insert CSS
+ * 01. External CSS: HTML page must include a reference to the external CSS sheet file using the link element inside the head section. It must be saved with a .css extension.
+ * 02. Internal CSS: Internal CSS is used if one single HTML page has a unique style. The internal style is defined inside the <style> element inside the head section. 
+ * 03. Inline CSS: used to apply a unique style for a single element. To use inline styles, add the style attribute to the relevant element. The style attribute can contain any CSS property. 
+ * imp: the order: inline > internal(if the style is defined after the external link) > external > browser default
+ * imp: the order: inline > external (if teh style is defined before the external link) > internal > browser default
+ * so, inline style has the highest priority, and will override external and internal styles and browser defaults.
+ * 
+ * ***comment : 
+ */
